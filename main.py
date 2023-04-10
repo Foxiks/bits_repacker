a7f71140745f6f005d611a729e8ff2cf00cb1a25
Фукиш <egor158741@yandex.ru>
2023-04-10 11:37:41 +0300

    Update main.py

--- main.py
+++ main.py
@@ -18,6 +18,7 @@ def dn_packer(input_file, out_file, in_lb, out_lb, chunk_size):
         chunks = [data[i:i+int(in_lb)] for i in range(0, len(data), int(in_lb))]
         chunks = [str[:-int(int(in_lb)-int(out_lb))] for str in chunks]
         bitstring.BitArray(bin=''.join(chunks)).tofile(out_file)
+    return
 
 def up_packer(input_file, out_file, in_lb, out_lb, chunk_size):
     data_stream=bitstring.ConstBitStream(input_file)
@@ -30,7 +31,7 @@ def up_packer(input_file, out_file, in_lb, out_lb, chunk_size):
         chunks = [data[i:i+int(in_lb)] for i in range(0, len(data), int(in_lb))]
         chunks = [str1+str('0'*int(int(out_lb)-int(in_lb))) for str1 in chunks]
         bitstring.BitArray(bin=''.join(chunks)).tofile(out_file)
-        
+    return
 
 def input_file_dialog():
     global input_file_name
@@ -56,10 +57,8 @@ def main():
     input_file=open(str(input_file_name), 'rb')
     out_file=open(str(out_file_name), 'ab')
     if(int(in_lb)>int(out_lb)):
-
         dn_packer(input_file=input_file, chunk_size=chunk_size, in_lb=in_lb, out_lb=out_lb, out_file=out_file)
     if(int(in_lb)<int(out_lb)):
-
         up_packer(input_file=input_file, chunk_size=chunk_size, in_lb=in_lb, out_lb=out_lb, out_file=out_file)
     if(int(in_lb)==int(out_lb)):
         None
@@ -73,13 +72,13 @@ if(__name__ == '__main__'):
     lbl.grid(column=0, row=0)
     txt1 = Entry(window,width=33)
     txt1.grid(column=1, row=0, columnspan=2)
-    btns = Button(window, text="Open", command=input_file_dialog, width=8) #command=input_file_dialog -> text to txt1
+    btns = Button(window, text="Open", command=input_file_dialog, width=8)
     btns.grid(column=3, row=0)
     lbl1 = Label(window, text="Output file:")
     lbl1.grid(column=0, row=1)
     txt2 = Entry(window,width=33)
     txt2.grid(column=1, row=1, columnspan=2)
-    btns1 = Button(window, text="Select", command=out_file_dialog, width=8) #command=out_file_dialog -> text to txt2
+    btns1 = Button(window, text="Select", command=out_file_dialog, width=8)
     btns1.grid(column=3, row=1)
     lbl2 = Label(window, text="Chunk size (bits):")
     lbl2.grid(column=0, row=3)
@@ -93,7 +92,7 @@ if(__name__ == '__main__'):
     lbl4.grid(column=2, row=2)
     txt5 = Entry(window,width=10)
     txt5.grid(column=3, row=2)
-    btns2 = Button(window, text="Start", command=main, width=8) #command=start
+    btns2 = Button(window, text="Start", command=main, width=8)
     btns2.grid(column=3, row=3)
     lbl5 = Label(window, text="Bits repacker [by UB1QBJ]")
     lbl5.grid(column=2, row=3)
